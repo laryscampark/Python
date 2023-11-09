ce7deb5876bc35870cb07dcea4d3a8c8cc8cd8d3
Laryssa Peçanha <laryssascampark@gmail.com>
2023-11-09 20:35:26 -0300

    exercicio sobre idade e ano de nascimento para saber se é maior de idade

---
+++ Logica de Programação/idadeMaior.py
@@ -0,0 +1,15 @@
+#Exercicio para saber se a pessoa é maior de idade e ano de nascimento e sua altura
+
+nome = 'Laryssa'
+sobrenome = 'Peçanha de Oliveira Barreto da Silva'
+idade = 26
+ano_nasc = 2023 - idade
+maior_de_idade = idade >= 18
+altura = 1.67
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento:', ano_nasc)
+print('É maior de idade?', maior_de_idade)
+print('Altura: ', altura)
