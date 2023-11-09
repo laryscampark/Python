67dc5e60ffc300994e6b3302192470e694f3a50c
Laryssa Peçanha <laryssascampark@gmail.com>
2023-11-09 20:49:38 -0300

    ajuste do codigo

--- Logica de Programação/calculoIMC.py
+++ Logica de Programação/calculoIMC.py
@@ -6,6 +6,3 @@ imc = peso / altura **2
 print(nome, 'tem', altura, 'de altura',)
 print('pesa ',peso,'quilos e seu imc é',)
 print(imc)
-#Laryssa tem 1.67 de altura
-#pesa 62 quilos e seu imc é
-# 22222
